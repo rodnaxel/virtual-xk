5f9c23e2dc1f6ff067641a8050c31bd4e345cd95
Aleksandr <alvonrims@gmail.com>
2026-06-25 12:54:02 +0300

    feat: add send response

--- main.py
+++ main.py
@@ -85,10 +85,18 @@ class SerialListener(threading.Thread):
                     continue
                 # Валидация пакета
                 # Обработка пакета
-                # Отправка ответа
-                # Логгированиe
+                parsed_data = self.parse_packet(packet)
                 
+                # Формирование ответа на основе данных из CSV
+                csv_row = next(self._csv_iter)
+                print(f"Using CSV row: {csv_row.get("glub")}")
+                response = self.create_response(parsed_data, csv_row)
                 
+                # Отправка ответа обратно в последовательный порт
+                self._serial.write(response)
+                
+                # Логирование отправленного ответа
+                #logger.debug(f"Sent response: {created_response.hex()}")              
         finally:
             if self._serial and self._serial.is_open:
                 self._serial.close()
@@ -105,6 +113,57 @@ class SerialListener(threading.Thread):
         logger.debug(f"Received packet: {len(data)} : {data.hex()}")
         return data
     
+    def parse_packet(self, packet: bytes) -> dict:
+        """Парсинг пакета данных и возврат словаря с данными."""
+        parsed_data = {
+            "SOP": packet[0],
+            "ID": packet[1],
+            "mode": packet[2],
+            "depth_range": packet[3],
+            "TGA ratio A": packet[4],
+            "gain": packet[5],
+            "TGA ratio B": packet[6],
+            "reverb timeout": packet[7],
+            "TGA table": packet[8],
+            "sound speed": packet[9] << 8 | packet[10],
+        }
+        logger.debug(f"Parsed packet data: {parsed_data}")
+        return parsed_data
+    
+    def create_response(self, parsed_data: dict, csv_row: dict) -> bytes:
+        """Создание ответа на основе данных из CSV."""
+        
+        response = bytearray(134)
+        response[0] = parsed_data["SOP"]  
+        response[1] = parsed_data["depth_range"]  
+        
+        response[2] = parsed_data["gain"]  
+        response[3] = 0         # число коррелированных стопов
+        response[4] = 0         # число принятых стопов
+        response[5] = 0x00      # резерв
+        
+        depth = int(csv_row.get("glub"))
+        response[6] = (depth >> 8) & 0xff  # старший байт глубины
+        response[7] = depth & 0xff         # младший байт глубины
+        
+        amplitude = int(csv_row.get("ampl"))
+        response[8] = (amplitude >> 8) & 0xff  # старший байт амплитуды
+        response[9] = amplitude & 0xff         # младший байт амплитуды
+        
+        duration = int(float(csv_row.get("lenth")))
+        response[10] = (duration >> 8) & 0xff  # старший байт длительности
+        response[11] = duration & 0xff         # младший байт длительности
+        
+        # Заполнение точек глубины и амплитуды
+        for i in range(12, 132):
+            response[i] = 0x00  # резервные байты
+        
+        response[132] = 0x0d  # резерв
+        response[133] = 0x0a  # резерв
+        
+        logger.debug(f"Created response from CSV data: {response.hex()}")
+        return bytes(response)
+    
 def main():
     args = parse_args()
     print(f"Connecting to port: {args.port} with baudrate: {args.baudrate}")
@@ -114,6 +173,7 @@ def main():
 
     serial_listener = SerialListener(args.port, args.baudrate, data_iter)
     serial_listener.start()
+    
     
     try:
         while serial_listener.is_alive():
